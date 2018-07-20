431d3ef0971a770143120cd8d51a5555f7d39bb3
cgnerds <cgnerds@gmail.com>
2018-07-20 12:39:42 +0800

    Serializable特性。

---
+++ Assets/Scripts/SerializableClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+
+[Serializable]
+public class SerializableClass{
+    public string name;
+    public int age;
+    public bool isHero;
+
+    public SerializableClass(string name, int age, bool isHero)
+    {
+        this.name = name;
+        this.age = age;
+        this.isHero = isHero;
+    }
+}
---
+++ Assets/Scripts/SerializableTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SerializableTest : MonoBehaviour {
+    public SerializableClass serializableClass;
+
+	// Use this for initialization
+	void Start () {
+        this.serializableClass = new SerializableClass("chenjiadong", 26, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(10, 10, 150, 100), "Serialize"))
+        {
+            string fileName = "serializableClass.dat";
+            Stream fStream = new FileStream(fileName, FileMode.Create, FileAccess.ReadWrite);
+            BinaryFormatter binFormat = new BinaryFormatter();
+            binFormat.Serialize(fStream, this.serializableClass);
+            fStream.Close();
+            this.serializableClass.name = "yanliang";
+            Debug.Log("the class name is: " + this.serializableClass.name);
+        }
+
+        if (GUI.Button(new Rect(300, 10, 150, 100), "Deserialize"))
+        {
+            string fileName = "serializableClass.dat";
+            Stream fStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryFormatter binFormat = new BinaryFormatter();
+            this.serializableClass = binFormat.Deserialize(fStream) as SerializableClass;
+            fStream.Close();
+            this.serializableClass.name = "yanliang";
+            Debug.Log("after Deserialize the class name is: " + this.serializableClass.name);
+        }
+    }
+}
