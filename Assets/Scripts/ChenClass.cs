b8e2713310b5a852f90c9dcad3e17b7a202fdcac
cgnerds <cgnerds@gmail.com>
2018-07-20 14:45:11 +0800

    检测定制特性。

--- Assets/Scripts/ChenClass.cs
+++ Assets/Scripts/ChenClass.cs
@@ -4,6 +4,15 @@
 class ChenClass{
     public static void Main()
     {
+        ChenClass c = new ChenClass();
+    }
 
+    public ChenClass()
+    {
+        // 判断ChenClass类是否应用了CustomAttribute特性，如果是则打印出ChenClass
+        if(this.GetType().IsDefined(typeof(CustomAttribute), false))
+        {
+            System.Console.WriteLine("ChenClass");
+        }
     }
 }
