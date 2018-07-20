a13b61a05dab8c35a551a34e951851fc3940a6eb
cgnerds <cgnerds@gmail.com>
2018-07-20 12:40:47 +0800

    定制特性到底是谁。

---
+++ Assets/Scripts/AttributeTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Transform/DebugLogYourPlatform")]
+public class AttributeTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Application.platform.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
