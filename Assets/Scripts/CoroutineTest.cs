fe7e60a569d8b1e19f2dc6f0626dd5d59575653d
cgnerds <cgnerds@gmail.com>
2018-07-23 15:56:59 +0800

    8.1.1 使用StartCoroutine方法开启协程。

--- Assets/Scripts/CoroutineTest.cs
+++ Assets/Scripts/CoroutineTest.cs
@@ -11,6 +11,7 @@ public class CoroutineTest : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        // GetComponent<MeshRenderer>().material.color = Color.yellow;
 		if(Input.GetKeyDown(KeyCode.F))
         {
             StartCoroutine("Fade");
@@ -20,9 +21,8 @@ public class CoroutineTest : MonoBehaviour {
     IEnumerator Fade()
     {
         Debug.Log("Fading...");
-        for(float f = 1f; f >= 0; f -= 0.1f)
+        for(float f = 1.0f; f >= 0; f -= 0.1f)
         {
-            //Color c = GetComponent<Renderer>().material.color;
             Color c = GetComponent<MeshRenderer>().material.color;
             c.a = f;
             GetComponent<MeshRenderer>().material.color = c;
