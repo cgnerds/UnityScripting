a078aee2690990f4e11eab86b50cd478a73c366b
cgnerds <cgnerds@gmail.com>
2018-07-23 18:50:24 +0800

    8.3 Unity 3D 协程背后的秘密--迭代器

---
+++ Assets/Scripts/EnumeratorTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class EnumeratorTest {
+    static void Main()
+    {
+        foreach (string s in GetEnumerableTest())
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+    static IEnumerable<string> GetEnumerableTest()
+    {
+        yield return "begin";
+
+        for(int i=0; i<10; i++)
+        {
+            yield return i.ToString();
+        }
+
+        yield return "end";
+    }
+}
---
+++ Assets/Scripts/EnumeratorTest2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class EnumeratorTest2 {
+
+    static IEnumerable<int> TestStateChange()
+    {
+        Console.WriteLine("----我TestStateChange是第一行代码");
+        Console.WriteLine("----我是第一个 yield return 前的代码");
+        yield return 1;
+        Console.WriteLine("----我是第一个 yield return 后的代码");
+        Console.WriteLine("----我是第二个 yield return 前的代码");
+        yield return 2;
+        Console.WriteLine("----我是第二个 yield return 后的代码");
+    }
+
+
+    static void Main()
+    {
+        Console.WriteLine("调用TestStateChange");
+        IEnumerable<int> iteratorable = TestStateChange();
+        Console.WriteLine("调用GetEnumerator");
+        IEnumerator<int> iterator = iteratorable.GetEnumerator();
+        Console.WriteLine("调用MoveNext()");
+        bool hasNext = iterator.MoveNext();
+        Console.WriteLine("是否有数据={0}; Current={1}", hasNext, iterator.Current);
+        Console.WriteLine("第二次调用MoveNext");
+        hasNext = iterator.MoveNext();
+        Console.WriteLine("是否有数据={0}; Current={1}", hasNext, iterator.Current);
+        Console.WriteLine("第三次调用MoveNext");
+        hasNext = iterator.MoveNext();
+        Console.WriteLine("是否有数据={0}", hasNext);
+    }
+}
