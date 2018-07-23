d35e0902e1cdd1271402c14c5880b5de695062b6
cgnerds <cgnerds@gmail.com>
2018-07-23 16:06:35 +0800

    8.1.2 使用StopCoroutine方法停止一个协程。

---
+++ Assets/Scripts/StopCoroutineTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopCoroutineTest : MonoBehaviour {
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        StartCoroutine("DoSomething", 2.0f);
+        yield return new WaitForSeconds(1);
+        StopCoroutine("DoSomething");
+    }
+
+    IEnumerator DoSomething(float someParameter)
+    {
+        while(true)
+        {
+            print("DoSomething Loop");
+            yield return null;
+        }
+    }
+}
