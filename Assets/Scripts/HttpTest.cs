7012b9bdef39d9b39a48b908515249a9e5a5548a
cgnerds <cgnerds@gmail.com>
2018-07-23 21:06:40 +0800

    8.4 WWW和协程。

---
+++ Assets/Scripts/HttpTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HttpTest : MonoBehaviour {
+    private Action<WWW> onSuccess;
+   
+	// Use this for initialization
+	void Start () {
+        this.onSuccess += this.SuccessMethod;
+        HttpWrapper hw = GetComponent<HttpWrapper>();
+        hw.GET("http://img.juimg.com/tuku/yulantu/140703/330746-140F302191731.jpg", this.onSuccess);
+	}
+
+    private void SuccessMethod(WWW www)
+    {
+        if (www == null)
+            return;
+        Texture tex = www.texture;
+        GetComponent<Renderer>().material.mainTexture = tex;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/WWWTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WWWTest : MonoBehaviour {
+
+    public string url = "http://img.juimg.com/tuku/yulantu/140703/330746-140F302191731.jpg";
+
+    IEnumerator Start()
+    {
+        WWW www = new WWW(url);
+        yield return www;
+        Debug.Log("Image Downloaded.");
+        GetComponent<Renderer>().material.mainTexture = www.texture;
+    }
+}
