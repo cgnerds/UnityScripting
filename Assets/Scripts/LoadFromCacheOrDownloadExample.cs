ef65c575137be599c9f38725d3fe7a8d6c619938
cgnerds <cgnerds@gmail.com>
2018-07-24 21:09:00 +0800

    11.5 使用WWW类加载资源。

---
+++ Assets/Scripts/LoadFromCacheOrDownloadExample.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadFromCacheOrDownloadExample : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+        var www = WWW.LoadFromCacheOrDownload("http://myserver.com/myassetBundle.unity3d", 5);
+        yield return www;
+        if(!string.IsNullOrEmpty(www.error))
+        {
+            Debug.Log(www.error);
+            yield return null;
+        }
+        var myLoadedAssetBundle = www.assetBundle;
+        var asset = myLoadedAssetBundle.mainAsset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/WWWFormScore.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WWWFormScore : MonoBehaviour {
+
+    string highscore_url = "http://www.my-site.com/highscores.pl";
+    string playName = "Player 1";
+    int score = -1;
+
+	// Use this for initialization
+	IEnumerator Start () {
+        WWWForm form = new WWWForm();
+        form.AddField("game", "MyGameName");
+        form.AddField("playerName", playName);
+        form.AddField("score", score);
+
+        WWW download = new WWW(highscore_url, form);
+
+        yield return download;
+
+        if(!string.IsNullOrEmpty(download.error))
+        {
+            print("Error downloading: " + download.error);
+        }
+        else
+        {
+            Debug.Log(download.text);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
