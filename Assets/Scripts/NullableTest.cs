acadf2274c900ea806d0947e24235739c493bcf4
cgnerds <cgnerds@gmail.com>
2018-07-24 09:57:28 +0800

    9.3 使用可空值类型。

---
+++ Assets/Scripts/NullableTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+
+public class NullableTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Nullable<Int32> testInt = 999;
+        Nullable<Int32> testNull = null;
+        Debug.Log("testInt has value: " + testInt.HasValue);
+        Debug.Log("testInt value: " + testInt.Value);
+        Debug.Log("testInt value: " + (Int32)testInt);
+        Debug.Log("testNull has value: " + testNull.HasValue);
+        Debug.Log("testNull value: " + testNull.GetValueOrDefault());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
