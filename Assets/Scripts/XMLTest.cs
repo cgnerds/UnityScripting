30b2acb3c9edbf085c7f77c2b825dc5f266be59b
cgnerds <cgnerds@gmail.com>
2018-07-24 16:22:53 +0800

    11.1 假如我想在编辑器里动态读取文件。

---
+++ Assets/Scripts/XMLTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Linq;
+
+public class XMLTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        XElement result = LoadXML("Assets/Xml/Test.xml");
+        Debug.Log(result.ToString());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    XElement LoadXML(string path)
+    {
+        XElement xml = XElement.Load(path);
+        return xml;
+    }
+}
