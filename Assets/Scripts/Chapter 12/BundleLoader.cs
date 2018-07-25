e5bcbd71c75987c9d5d16c5f81745cfe6d1500f3
cgnerds <cgnerds@gmail.com>
2018-07-25 10:28:24 +0800

    Chapter 12 Finished。

---
+++ Assets/Scripts/Chapter 12/BundleLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BundleLoader : MonoBehaviour {
+
+    public string url;
+    public int version;
+
+	IEnumerator Start () {
+        while (!Caching.ready)
+            yield return null;
+
+        WWW www = WWW.LoadFromCacheOrDownload(url, version);
+        yield return www;
+        if(!string.IsNullOrEmpty(www.error))
+        {
+            Debug.Log(www.error);
+            yield return null;
+        }
+
+        AssetBundle assetBundle = www.assetBundle;
+        GameObject gameObject = assetBundle.LoadAsset<GameObject>("Cube");
+        Instantiate(gameObject);
+        assetBundle.Unload(false);
+    }
+
+}
---
+++ Assets/Scripts/Chapter 12/ExportAssetBundles.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ExportAssetBundles {
+
+    [MenuItem("Assets/Build AssetBundle From Selection - Track dependencies")]
+    [System.Obsolete]
+    static void ExportResource()
+    {
+        string path = "Assets/myAssetBundle.unity3d";
+        if(path.Length != 0)
+        {
+            Object[] selection = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+            BuildPipeline.BuildAssetBundle(Selection.activeObject, selection,
+                path, BuildAssetBundleOptions.CollectDependencies, BuildTarget.StandaloneWindows);
+            Selection.objects = selection;
+        }
+    }
+
+    [MenuItem("Assets/Build AssetBundle From Selection - No dependency tracking")]
+    [System.Obsolete]
+    static void ExportResourceNoTrack()
+    {
+        string path = "Assets/myAssetBundle.unity3d";
+        if(path.Length != 0)
+        {
+            BuildPipeline.BuildAssetBundle(Selection.activeObject, Selection.objects, 
+                path, BuildAssetBundleOptions.CollectDependencies, BuildTarget.StandaloneWindows);
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
